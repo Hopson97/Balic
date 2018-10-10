1de09c9a08272d750eedbbbd84b78a0c6e3d4e4b
Hopson97 <mhopson@hotmail.co.uk>
2018-10-10 08:58:29 +0100

    Finish the 'linear compressor'

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+bool isDifferent(sf::Color a, sf::Color b) {
+    return 
+        std::abs(a.r - b.r) > 50 ||
+        std::abs(a.g - b.g) > 50 ||    
+        std::abs(a.b - b.b) > 50;
+}
 
-void linearCompress(const sf::Image& originalImage, sf::Image& newImage) {
+void linearCompress(const sf::Image& originalImage, sf::Image& newImage, unsigned width, unsigned height) {
+    sf::Color activeColour;
+    bool isNewColourNeeded = true;
+    int newChnages = 0;
 
+    for (unsigned y = 0; y < height - 1; y++) {
+        for (unsigned x = 0; x < width - 1; x++) {
+            if (isNewColourNeeded) {
+                activeColour = originalImage.getPixel(x, y);
+                newImage.setPixel(x, y, activeColour);
+                isNewColourNeeded = false;
+            }
+
+            if (!isDifferent(activeColour, originalImage.getPixel(x + 1, y))) {
+                newImage.setPixel(x + 1, y, activeColour);
+                newChnages++;
+            }
+            else {
+                isNewColourNeeded = true;
+            }
+            
+        }
+    }
+    std::cout << "New compress poop: " << newChnages << "\n";
 }
 
 int main(int argc, char** argv) {
@@ -28,5 +56,7 @@ int main(int argc, char** argv) {
     sf::Image newImage;
     newImage.create(width, height);
 
-    linearCompress(originalImage, newImage);
+    linearCompress(originalImage, newImage, width, height);
+
+    newImage.saveToFile("out.jpg");
 }
