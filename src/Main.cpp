e2fe0c1239aa98e13ac1c98c57420cd2736439d1
Hopson97 <mhopson@hotmail.co.uk>
2018-10-11 07:40:29 +0100

    Increase delay for the linear compress

--- src/Main.cpp
+++ src/Main.cpp
@@ -39,7 +39,7 @@ namespace {
                     isNewColourNeeded = true;
                 }
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
     }
 
